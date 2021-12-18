d8b7f40feca4a7dbc9f4415c11b99f99f2daeeda
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-19 00:56:37 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "И поэтому все так произошло";
+
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
